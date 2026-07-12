0f86fc968239587dbbdc240e2ce6d5c6d1c647b2
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-07-12 12:13:51 +0530

    feat: refactor tests to use withFreshQueue for setup and improve assertions

--- test/Tests/PGQueuer.hs
+++ test/Tests/PGQueuer.hs
@@ -43,18 +43,29 @@ runTests =
             , logAndListJobs
             ]
 
+withFreshQueue :: (QueueManager -> IO a) -> IO a
+withFreshQueue action = do
+    let conStr = "postgresql://queue_user:queue_pass@localhost:5432/queue_db"
+    let dbSetting = defaultDBSettings
+    queueMgrId <- nextRandom
+    withQueueManager conStr dbSetting queueMgrId $ \qm -> do
+        uninstallSchema qm
+        installSchema qm
+        action qm
+
+expectOne :: String -> [a] -> (a -> Assertion) -> Assertion
+expectOne message values assertion =
+    case listToMaybe values of
+        Nothing -> assertFailure message
+        Just value -> assertion value
+
 schemaTests :: TestTree
 schemaTests = do
     testGroup "Schema related tests" [checkInstallUninstallSchema]
 
 checkInstallUninstallSchema :: TestTree
 checkInstallUninstallSchema = testCase "installed schema should should be deleted safely" $ do
-    let conStr = "postgresql://queue_user:queue_pass@localhost:5432/queue_db"
-    let dbSetting = defaultDBSettings
-    queueMgrId <- nextRandom
-    withQueueManager conStr dbSetting queueMgrId $ \qm -> do
-        uninstallSchema qm -- Uninstall to make it idomatic
-        _ <- installSchema qm
+    withFreshQueue $ \qm -> do
         schemaInstalled <- verifyStructure qm
         assertBool "Schema is installed and verified" (isRight schemaInstalled)
         uninstallSchema qm
@@ -63,49 +74,35 @@ checkInstallUninstallSchema = testCase "installed schema should should be delete
 
 entrypointTests :: TestTree
 entrypointTests = testCase "Adding an entrypoint in qm adds entrypoint" $ do
-    let conStr = "postgresql://queue_user:queue_pass@localhost:5432/queue_db"
-    let dbSetting = defaultDBSettings
-    queueMgrId <- nextRandom
-    withQueueManager conStr dbSetting queueMgrId $ \qm -> do
+    withFreshQueue $ \qm -> do
         let ep = Entrypoint "hello"
         qm1 <- registerEntrypoint qm ep (\_ -> pure ())
         let entrypoints = qmEntrypoints qm1
-        assertBool "qm should contain entrypoint hello" $ do
-            isJust (Map.lookup "hello" entrypoints)
+        assertBool "qm should contain entrypoint hello" $ isJust (Map.lookup "hello" entrypoints)
 
 enqueueDequeueJobs :: TestTree
 enqueueDequeueJobs =
     testCase "Enqueue jobs should have entry in PGQ table" $ do
-        let conStr = "postgresql://queue_user:queue_pass@localhost:5432/queue_db"
-        let dbSetting = defaultDBSettings
-        queueMgrId <- nextRandom
-        withQueueManager conStr dbSetting queueMgrId $ \qm -> do
-            uninstallSchema qm
-            _ <- installSchema qm
+        withFreshQueue $ \qm -> do
             let ep = Entrypoint "hello"
             let params = [EntrypointExecutionParameter ep 0]
             qm1 <- registerEntrypoint qm ep (\_ -> pure ())
             _ <- enqueue qm1 ep Nothing 0 Nothing Nothing Nothing
             stats <- getQueueSize qm1
-            case listToMaybe stats of
-                Nothing -> assertFailure "List is empty"
-                Just stat -> assertBool "Should contain exactly one entry" $ statsCount stat == 1
+            expectOne "List is empty" stats $ \stat ->
+                assertBool "Should contain exactly one entry" $ statsCount stat == 1
 
-            _ <- dequeue qm1 20 params Nothing 3
-            stats1 <- getQueueSize qm1
-            case listToMaybe stats1 of
-                Nothing -> assertFailure "statistics are empty"
-                Just stat -> assertBool "Should get picked up" $ statsStatus stat == Picked
+            pickedJobs <- dequeue qm1 20 params Nothing 3
+            expectOne "dequeue returned no jobs" pickedJobs $ \job -> do
+                assertEqual "dequeued job should be picked" Picked (jobStatus job)
+                status <- listJobStatusById qm1 [jobId job]
+                expectOne "job status lookup failed" status $ \(_, jobStatus') ->
+                    assertEqual "job should be picked" Picked jobStatus'
 
 enqueueDequeueMultipleJobs :: TestTree
 enqueueDequeueMultipleJobs =
     testCase "Enqueue multiple jobs dequeue them" $ do
-        let conStr = "postgresql://queue_user:queue_pass@localhost:5432/queue_db"
-        let dbSetting = defaultDBSettings
-        queueMgrId <- nextRandom
-        withQueueManager conStr dbSetting queueMgrId $ \qm -> do
-            uninstallSchema qm
-            _ <- installSchema qm
+        withFreshQueue $ \qm -> do
             let ep = Entrypoint "hello"
             let params = [EntrypointExecutionParameter ep 0]
             qm1 <- registerEntrypoint qm ep (\_ -> pure ())
@@ -119,26 +116,22 @@ enqueueDequeueMultipleJobs =
                     (replicate 10 Nothing)
                     (replicate 10 Nothing)
             stats <- getQueueSize qm1
-            case listToMaybe stats of
-                Nothing -> assertFailure "statistics are empty"
-                Just stat -> assertEqual "Should contain exactly 10 entries" (statsCount stat) 10
+            expectOne "statistics are empty" stats $ \stat ->
+                assertEqual "Should contain exactly 10 entries" 10 (statsCount stat)
 
-            _ <- dequeue qm1 20 params Nothing 3
+            pickedJobs <- dequeue qm1 20 params Nothing 3
+            assertEqual "Should dequeue 10 jobs" 10 (length pickedJobs)
+            assertBool "All jobs should be picked" (all ((== Picked) . jobStatus) pickedJobs)
             stats1 <- getQueueSize qm1
-            case listToMaybe stats1 of
-                Nothing -> assertFailure "statistics are empty"
-                Just stat -> assertEqual "Should contain exactly zero entry" (statsStatus stat) Picked
+            expectOne "statistics are empty" stats1 $ \stat ->
+                assertEqual "picked jobs should be tracked" Picked (statsStatus stat)
 
 logAndListJobs :: TestTree
 logAndListJobs =
-    testCase "Update job status to failed and cancelled" $ do
-        let conStr = "postgresql://queue_user:queue_pass@localhost:5432/queue_db"
-        let dbSetting = defaultDBSettings
-        queueMgrId <- nextRandom
-        withQueueManager conStr dbSetting queueMgrId $ \qm -> do
-            uninstallSchema qm
-            _ <- installSchema qm
+    testCase "Update job status, retry, requeue and clear queue" $ do
+        withFreshQueue $ \qm -> do
             let ep = Entrypoint "hello"
+            let params = [EntrypointExecutionParameter ep 0]
             qm1 <- registerEntrypoint qm ep (\_ -> pure ())
             jobIds <-
                 enqueueMultiple
@@ -149,22 +142,36 @@ logAndListJobs =
                     (replicate 2 Nothing)
                     (replicate 2 Nothing)
                     (replicate 2 Nothing)
-            case jobIds of
-                (job1 : job2 : _) -> do
-                    logJobs qm1 [(job1, Failed, Nothing)]
-                    failedJobs <- listFailedJobs qm1 1
-                    case listToMaybe failedJobs of
-                        Nothing -> assertFailure "Need exactly one failed job"
-                        Just failedJob ->
-                            assertEqual
-                                "should be update jobid"
-                                job1
-                                (jobId failedJob)
-                    markJobAsCancelled qm1 [job2]
-                    cancelledJobStats <- listJobStatusById qm1 [job2]
-                    case listToMaybe cancelledJobStats of
-                        Nothing -> assertFailure "Need exactly one cancelled job"
-                        Just (jId, jStatus) -> do
-                            assertEqual "Round trup for JobStatusById" jId job2
-                            assertEqual "Status should be cancelled" jStatus Canceled
-                _ -> assertFailure "There should be at least two jobs"
+            assertEqual "enqueueMultiple should return two job ids" 2 (length jobIds)
+            pickedJobs <- dequeue qm1 2 params Nothing 3
+            case pickedJobs of
+                [job1, job2] -> do
+                    updateHeartbeat qm1 [jobId job1]
+                    heartbeatStatus <- listJobStatusById qm1 [jobId job1]
+                    expectOne "heartbeat status lookup failed" heartbeatStatus $ \(_, status) ->
+                        assertEqual "picked job should stay picked" Picked status
+
+                    logJobs qm1 [(jobId job1, Failed, Nothing)]
+                    failedJobs <- listFailedJobs qm1 10
+                    expectOne "Need exactly one failed job" failedJobs $ \failedJob -> do
+                        assertEqual "failed job id mismatch" (jobId job1) (jobId failedJob)
+                        retryJob qm1 failedJob 0 Nothing
+
+                    retryStatus <- listJobStatusById qm1 [jobId job1]
+                    expectOne "retry status lookup failed" retryStatus $ \(_, status) ->
+                        assertEqual "retried job should be queued" Queued status
+
+                    requeueJobs qm1 [jobId job2]
+                    requeueStatus <- listJobStatusById qm1 [jobId job2]
+                    expectOne "requeue status lookup failed" requeueStatus $ \(_, status) ->
+                        assertEqual "requeued job should be queued" Queued status
+
+                    markJobAsCancelled qm1 [jobId job2]
+                    cancelledStatus <- listJobStatusById qm1 [jobId job2]
+                    expectOne "cancelled status lookup failed" cancelledStatus $ \(_, status) ->
+                        assertEqual "cancelled job should be cancelled" Canceled status
+
+                    clearQueue qm1 (Just [ep])
+                    remaining <- getQueueSize qm1
+                    assertBool "queue should be empty after clearQueue" (null remaining)
+                _ -> assertFailure "There should be exactly two picked jobs"
