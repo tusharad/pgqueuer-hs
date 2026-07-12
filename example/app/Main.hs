415457bba61f039dddba5830e5a0bb64cf0bec73
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-07-12 19:41:01 +0530

    feat: made pgqueuer compatible with python

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -1,7 +1,16 @@
 module Main (main) where
 
-import HPCScheduler
-import Simple ()
+import System.Environment (getArgs)
+
+import qualified HPCScheduler
+import qualified Python
+import qualified Simple
 
 main :: IO ()
-main = runApp
+main = do
+    args <- getArgs
+    case args of
+        ["simple"] -> Simple.runApp
+        ["hpcscheduler"] -> HPCScheduler.runApp
+        ["python"] -> Python.runApp
+        _ -> putStrLn "usage: example-exe [simple|hpcscheduler|python]"
